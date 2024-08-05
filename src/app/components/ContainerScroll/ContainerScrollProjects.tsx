079b621644e60f4c49613658c1ca6112d1260f7f
Antony rossi de jesus <antonyrossidejesus@gmail.com>
2024-08-05 01:08:06 -0300

    atualizacao projects

--- src/app/components/ContainerScroll/ContainerScrollProjects.tsx
+++ src/app/components/ContainerScroll/ContainerScrollProjects.tsx
@@ -5,15 +5,15 @@ import { ContainerScroll } from "../ui/container-scroll-animation";
 
 export function HeroScrollDemo() {
   return (
-    <div className="flex flex-col overflow-hidden md:pb-20">
+    <div className="flex flex-col overflow-hidden md:pb-20 mt-4">
       <ContainerScroll
         titleComponent={
           <>
-            <h1 className="text-4xl font-semibold text-black dark:text-white">
+            <h1 className="text-3xl md:text-4xl font-semibold text-black dark:text-white">
               landing page
             </h1>
             <br />
-            <span className="text-5xl text-gray-600 md:text-[6rem] font-bold mt-1 leading-none">
+            <span className="text-4xl text-gray-600 sm:text-[4rem] md:text-[4.5rem] lg:text-[6rem] font-bold mt-1 leading-none">
               SEU PRODUTO <span className="text-green-500">VENDE MAIS</span>
             </span>
           </>
